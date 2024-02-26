800a058ed5e687d589a5d41998f0459cb3099375
seongeun <jdc05124@pusan.ac.kr>
2024-02-26 10:20:49 +0900

    media 제거

--- side_project/settings.py
+++ side_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-k=f$p!b9$$)dx9enqd(k$!!!x86+td(4pv)cn*!wx10i)y9&#c
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.0.10','127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
